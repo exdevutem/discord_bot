c334ee29af0c8524c5e4b72b454d4ff57c877f97
Rafael Morales Venegas <rmorales@utem.cl>
2022-11-22 13:10:12 -0300

    Bot de Discord básico.
    El bot debiese responder a cualquier Ping que se le haga con un "Pong!".

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # discord_bot
 El bot de discord creado para el club ExDev
+
+## Requisitos
+Este bot fue construido con Rust, y el proyecto es manejado por Cargo.
+Para poder compilar este proyecto se necesitan ambas herramientas.
+Puedes descargarlas desde [el siguiente link](https://www.rust-lang.org/es).
+
+## Dependencias
+Se utiliza el framework para bot de Discord 'Serenity', el cual tiene una 
+fuerte dependencia de 'Tokio' para la construccion de funciones asincronas.
+
+## Como compilar
+De no haberse roto las dependencias, deberia ser posible compilar con
+el comando tipico de cargo:
+
+```bash
+$ cargo build
+```
+
+Para poder hacer correr el bot es necesario proveerle tu token de Discord.
+La forma de hacerlo es a traves de una variable de ambiente.
+Para esto, copia el archivo .env.example con el nombre '.env', y rellena
+con los valores que correspondan. Luego, ejecuta el comando:
+
+```bash
+$ cargo run 
+```
+
+Recuerda que nunca deberias commitear tu archivo .env! esta incluido en el
+gitignore por buena razon, y es que ahi es donde iran tus contraseñas o
+credenciales de cualquier tipo.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+use serenity::async_trait;
+use serenity::framework::standard::macros::{command, group};
+use serenity::framework::standard::{CommandResult, StandardFramework};
+use serenity::model::channel::Message;
+use serenity::prelude::*;
+
+use dotenv::dotenv;
+
+#[group]
+#[commands(ping)]
+struct General;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {}
+
+#[tokio::main]
+async fn main() {
+    // Carga variables del ambiente desde el archivo .env
+    dotenv().ok();
+
+    let framework = StandardFramework::new()
+        .configure(|c| c.prefix('!'))
+        .group(&GENERAL_GROUP);
+
+    let token = env::var("DISCORD_TOKEN").expect("Missing Discord Token");
+    let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
+
+    let mut client = Client::builder(token, intents)
+        .event_handler(Handler)
+        .framework(framework)
+        .await
+        .expect("Error creating Client");
+
+    if let Err(why) = client.start().await {
+        println!("An error ocurred while running the client: {:?}", why);
+    }
+}
+
+#[command]
+async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.reply(ctx, "Pong!").await?;
+
+    Ok(())
 }
