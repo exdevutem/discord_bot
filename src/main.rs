c56bebbd58e0d5970439419779f46b7c435591e8
Rafael Morales <rmorales@utem.cl>
2022-11-23 00:28:45 -0300

    Modificado bot para utilizar Slash Commands
    Por alguna razon requiere que el Guild ID este en las variables de ambiente
    No estoy muy seguro de como obtenerlo de manera dinamica...
    Voy a seguir leyendo.

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,87 @@
+mod commands;
+
 use std::env;
 
 use serenity::async_trait;
-use serenity::framework::standard::macros::{command, group};
-use serenity::framework::standard::{CommandResult, StandardFramework};
+use serenity::model::application::interaction::{Interaction, InteractionResponseType};
 use serenity::model::channel::Message;
+use serenity::model::gateway::Ready;
+use serenity::model::id::GuildId;
 use serenity::prelude::*;
 
 use dotenv::dotenv;
 
-#[group]
-#[commands(ping)]
-struct General;
-
 struct Handler;
 
 #[async_trait]
-impl EventHandler for Handler {}
+impl EventHandler for Handler {
+    async fn message(&self, context: Context, msg: Message) {
+        if msg.content == "!messageme" {
+            let dm = msg.author.dm(&context, |m| m.content("hola gil")).await;
+
+            if let Err(why) = dm {
+                println!("Error al mandar mensaje al usuario: {:?}", why);
+            }
+        }
+    }
+
+    async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
+        if let Interaction::ApplicationCommand(command) = interaction {
+            println!("Received command interaction: {:#?}", command);
+
+            let content = match command.data.name.as_str() {
+                "ping" => commands::ping::run(&command.data.options),
+                _ => "not implemented :(".to_string(),
+            };
+
+            if let Err(why) = command
+                .create_interaction_response(&ctx.http, |response| {
+                    response
+                        .kind(InteractionResponseType::ChannelMessageWithSource)
+                        .interaction_response_data(|message| message.content(content))
+                })
+                .await
+            {
+                println!("Cannot respond to slash command: {}", why);
+            }
+        }
+    }
+
+    async fn ready(&self, ctx: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+
+        let guild_id = GuildId(
+            env::var("GUILD_ID")
+                .expect("Expected GUILD_ID in environment")
+                .parse()
+                .expect("GUILD_ID must be an integer"),
+        );
+
+        let commands = GuildId::set_application_commands(&guild_id, &ctx.http, |commands| {
+            commands.create_application_command(|command| commands::ping::register(command))
+        })
+        .await;
+
+        println!(
+            "I now have the following guild slash commands: {:#?}",
+            commands
+        );
+    }
+}
 
 #[tokio::main]
 async fn main() {
     // Carga variables del ambiente desde el archivo .env
     dotenv().ok();
 
-    let framework = StandardFramework::new()
-        .configure(|c| c.prefix('!'))
-        .group(&GENERAL_GROUP);
-
     let token = env::var("DISCORD_TOKEN").expect("Missing Discord Token");
-    let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
 
-    let mut client = Client::builder(token, intents)
+    let mut client = Client::builder(token, GatewayIntents::empty())
         .event_handler(Handler)
-        .framework(framework)
         .await
         .expect("Error creating Client");
 
     if let Err(why) = client.start().await {
         println!("An error ocurred while running the client: {:?}", why);
     }
-}
-
-#[command]
-async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
-    msg.reply(ctx, "Pong!").await?;
-
-    Ok(())
 }
